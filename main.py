221553011788d707a1246cde05b05327d310a6aa
Vovchek <Vlad.aks03@mail.ru>
2021-01-25 22:07:27 +0500

    до меня дошло, шо я даун

--- main.py
+++ main.py
@@ -10,34 +10,34 @@ class Figure:
     def get_im_st(self):
         return FIG_IM_ST[self.__class__]
 
-    def get_step_pos(self):
-        return None
+    def get_step_pos(self, bord: set):
+        return set()
 
-    def get_attack_pos(self):
-        return None
+    def get_attack_pos(self, bord: set):
+        return set()
 
     def ded(self):
         self.is_ded = True
 
 
 class StepAttack(Figure):
-    def get_attack_pos(self):
-        self.get_step_pos()
+    def get_attack_pos(self, bord: set):
+        return self.get_step_pos(bord)
 
 
 class Pawn(Figure):
-    def get_step_pos(self):
-        return self.x, self.y - self.color
+    def get_step_pos(self, bord: set):
+        return {(self.x, self.y - self.color)} & bord
 
-    def get_attack_pos(self):
-        y = self.get_step_pos()[1]
-        return ((self.x - 1, y),
-                (self.x + 1, y))
+    def get_attack_pos(self, bord: set):
+        y = self.get_step_pos(bord).pop()[1]
+        return {(self.x - 1, y),
+                (self.x + 1, y)} & bord
 
 
 class Knight(StepAttack):
-    def get_step_pos(self):
-        return ((self.x - 1, self.y - 2),
+    def get_step_pos(self, bord: set):
+        return {(self.x - 1, self.y - 2),
                 (self.x + 1, self.y - 2),
 
                 (self.x + 2, self.y - 1),
@@ -47,32 +47,32 @@ class Knight(StepAttack):
                 (self.x + 1, self.y + 2),
 
                 (self.x - 2, self.y - 1),
-                (self.x - 2, self.y + 1))
+                (self.x - 2, self.y + 1)} & bord
 
 
 class Rook(StepAttack):
-    def get_step_pos(self):
-        return tuple([(self.x, self.y + i) for i in range(-7, 8) if i != 0] +
-                     [(self.x + i, self.y) for i in range(-7, 8) if i != 0])
+    def get_step_pos(self, bord: set):
+        return set([(self.x, self.y + i) for i in range(-7, 8) if i != 0] +
+                   [(self.x + i, self.y) for i in range(-7, 8) if i != 0]) & bord
 
 
 class Bishop(StepAttack):
-    def get_step_pos(self):
-        return tuple([(self.x + i, self.y + i) for i in range(-7, 8) if i != 0] +
-                     [(self.x + i, self.y - i) for i in range(-7, 8) if i != 0])
+    def get_step_pos(self, bord: set):
+        return set([(self.x + i, self.y + i) for i in range(-7, 8) if i != 0] +
+                   [(self.x + i, self.y - i) for i in range(-7, 8) if i != 0]) & bord
 
 
 class Queen(StepAttack):
-    def get_step_pos(self):
-        return Bishop(*self.pos, self.color).get_step_pos() + Rook(*self.pos, self.color).get_step_pos()
+    def get_step_pos(self, bord: set):
+        return Bishop(*self.pos, self.color).get_step_pos(bord) | Rook(*self.pos, self.color).get_step_pos(bord)
 
 
 class King(StepAttack):
-    def get_step_pos(self):
-        return [(self.x + i, self.y + j)
-                for i in range(-1, 2)
-                for j in range(-1, 2)
-                if (i, j) != (0, 0)]
+    def get_step_pos(self, bord: set):
+        return set([(self.x + i, self.y + j)
+                    for i in range(-1, 2)
+                    for j in range(-1, 2)
+                    if (i, j) != (0, 0)]) & bord
 
 
 class Bord:
