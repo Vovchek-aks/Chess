011bf868f52a0334eec9c945bfd97f04b9557a0c
Vovchek <Vlad.aks03@mail.ru>
2021-01-26 23:24:04 +0500

    вроде начал чинить

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ FIG_IM_ST = {}
 
 class Figure:
     def __init__(self, x, y, color):
-        self.pos = self.x, self.y = x, y
+        self.pos = self.y, self.x = y, x
         self.is_ded = False
         self.color = color
 
     def get_im_st(self):
-        return FIG_IM_ST[self.__class__]
+        return FIG_IM_ST.get(self.__class__, '@')
 
     def __str__(self):
         return str((self.__class__.__name__, self.pos, self.color))
@@ -16,74 +16,106 @@ class Figure:
     def __repr__(self):
         return self.__str__()
 
-    def get_step_pos(self, bord: set):
+    def get_step_pos(self, bord):
         return set()
 
-    def get_attack_pos(self, bord: set):
+    def get_attack_pos(self, bord):
         return set()
+
+    def go_pos(self, bord):
+        return self.get_step_pos(bord) | self.get_attack_pos(bord)
 
     def ded(self):
         self.is_ded = True
 
 
 class StepAttack(Figure):
-    def get_attack_pos(self, bord: set):
+    def get_attack_pos(self, bord):
         return self.get_step_pos(bord)
 
 
 class EmptyF(StepAttack):
-    def get_step_pos(self, bord: set):
+    def __init__(self, x, y, color=0):
+        super().__init__(x, y, 0)
+
+    def get_step_pos(self, bord):
         return set()
 
 
 class Pawn(Figure):
-    def get_step_pos(self, bord: set):
-        return {(self.x, self.y + self.color)} & bord
+    def get_step_pos(self, bord):
+        return {(self.x, self.y + self.color)[::-1]} & bord.b_get_free()
 
-    def get_attack_pos(self, bord: set):
+    def get_attack_pos(self, bord):
         y = self.y + self.color
-        return {(self.x - 1, y),
-                (self.x + 1, y)} & bord
+        return {(self.x - 1, y)[::-1],
+                (self.x + 1, y)[::-1]} & bord.b_get_all(self.color)
 
 
 class Knight(StepAttack):
-    def get_step_pos(self, bord: set):
-        return {(self.x - 1, self.y - 2),
-                (self.x + 1, self.y - 2),
+    def get_step_pos(self, bord):
+        return {(self.x - 1, self.y - 2)[::-1],
+                (self.x + 1, self.y - 2)[::-1],
 
-                (self.x + 2, self.y - 1),
-                (self.x + 2, self.y + 1),
+                (self.x + 2, self.y - 1)[::-1],
+                (self.x + 2, self.y + 1)[::-1],
 
-                (self.x - 1, self.y + 2),
-                (self.x + 1, self.y + 2),
+                (self.x - 1, self.y + 2)[::-1],
+                (self.x + 1, self.y + 2)[::-1],
 
-                (self.x - 2, self.y - 1),
-                (self.x - 2, self.y + 1)} & bord
+                (self.x - 2, self.y - 1)[::-1],
+                (self.x - 2, self.y + 1)[::-1]} & bord.b_get_all(self.color)
 
 
 class Rook(StepAttack):
-    def get_step_pos(self, bord: set):
-        return set([(self.x, self.y + i) for i in range(-7, 8) if i != 0] +
-                   [(self.x + i, self.y) for i in range(-7, 8) if i != 0]) & bord
+    def get_step_pos(self, bord):
+        r = set()
+        f = bord.b_get_free()
+        b = bord.b_get_busy(self.color)
+        ssum = lambda x, y, z: tuple([(x[g] + y[g]) * z for g in range(len(x))])
+        for p in [[1, 0], [0, 1], [0, -1], [-1, 0]]:
+            for i in range(8):
+                s = ssum(self.pos, p, i + 1)
+                if s in f:
+                    r.add(s)
+                elif s in b:
+                    r.add(s)
+                    break
+                else:
+                    break
+        return r
 
 
 class Bishop(StepAttack):
-    def get_step_pos(self, bord: set):
-        return set([(self.x + i, self.y + i) for i in range(-7, 8) if i != 0] +
-                   [(self.x + i, self.y - i) for i in range(-7, 8) if i != 0]) & bord
+    def get_step_pos(self, bord):
+        r = set()
+        f = bord.b_get_free()
+        b = bord.b_get_busy(self.color)
+        ssum = lambda x, y, z: tuple([(x[g] + y[g]) * z for g in range(len(x))])
+        for p in [[1, 1], [-1, 1], [1, -1], [-1, -1]]:
+            for i in range(8):
+                s = ssum(self.pos, p, i + 1)
+                if s in f:
+                    r.add(s)
+                elif s in b:
+                    r.add(s)
+                    break
+                else:
+                    break
+        return r
 
 
 class Queen(StepAttack):
-    def get_step_pos(self, bord: set):
-        return Bishop(*self.pos, self.color).get_step_pos(bord) | Rook(*self.pos, self.color).get_step_pos(bord)
+    def get_step_pos(self, bord):
+        return Bishop(*self.pos, self.color).get_attack_pos(bord) | Rook(*self.pos, self.color).get_attack_pos(bord)
 
 
 class King(StepAttack):
-    def get_step_pos(self, bord: set):
-        return set([(self.x + i, self.y + j)
+    def get_step_pos(self, bord):
+        return set([(self.x + i, self.y + j)[::-1]
                     for i in range(-1, 2)
                     for j in range(-1, 2)
-                    if (i, j) != (0, 0)]) & bord
+                    if (i, j) != (0, 0)]) & bord.b_get_all(self.color)
 
 
 class Bord:
@@ -92,14 +124,34 @@ class Bord:
 
         self.grid = [[sp[x](x, 0, 1) for x in range(len(sp))]] + \
                     [[Pawn(x, 1, 1) for x in range(8)]] + \
-                    [[EmptyF(x, y, 0) for x in range(8)] for y in range(2, 6)] + \
+                    [[EmptyF(x, y) for x in range(8)] for y in range(2, 6)] + \
                     [[Pawn(x, 6, -1) for x in range(8)]] + \
                     [[sp[x](x, 7, -1) for x in range(len(sp))]]
 
+    def __getitem__(self, item):
+        r = []
+        for i in self.grid:
+            r += i
+        return r[item]
+
+    def b_get_free(self):
+        r = set()
+        for i in self:
+            if not i.color:
+                r.add(i.pos)
+        return r
+
+    def b_get_busy(self, color):
+        r = set()
+        for i in self:
+            if i.color and i.color != color:
+                r.add(i.pos)
+        return r
+
+    def b_get_all(self, color):
+        return self.b_get_free() | self.b_get_busy(color)
 
 
+b = Bord()
 
-
-
-
-# print(*Bord().grid, sep='\n')
+print(b.grid[0][4].go_pos(b))
