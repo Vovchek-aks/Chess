0071a5bed6298cd64ef5e5a2747744e8fd88e096
Vovchek <Vlad.aks03@mail.ru>
2021-02-02 23:03:58 +0500

    вроде ок

--- ui.py
+++ ui.py
@@ -7,10 +7,9 @@ from ui_settings import *
 class UiBord(Bord):
     def __init__(self):
         super().__init__()
-        self.select = None
 
     def draw(self, sc, fn, x, y, s, color):
-        sc.blit(fn.render(f'Ход: {self.n_step}{" " * 10} Ходит  {"белый" if color == 1 else "чёрный"}', False, dk_gray),
+        sc.blit(fn.render(f'Ход: {self.n_step}{" " * 10} Ходит  {"белый" if color == 1 else "чёрный"}', False, black),
                 (x, y - fn.size('1')[1] - 10))
         c = 1
         for i in range(8):
@@ -21,19 +20,71 @@ class UiBord(Bord):
                 c *= -1
 
                 f = self.grid[i][j]
-                sc.blit(FIG_IM_ST[(f.__class__, f.color)], (x + s * j, y + s * i))
+                if f.__class__ != EmptyF:
+                    sc.blit(FIG_IM_ST[(f.__class__, f.color)], (x + s * j, y + s * i))
+
+            sc.blit(fn.render(str(8 - i), False, black), (x - 20, y + s // 2.5 + s * i))
+            sc.blit(fn.render(str(i + 1), False, black), (x + s // 2.5 + s * i, y + s * 8 + 10))
             c *= -1
+
+        sc.blit(fn.render('Потери:', False, black), (x + s * 8 + 40, y - fn.size('1')[1] - 10))
+        for i in range(len(self.lose[1])):
+            sc.blit(FIG_IM_ST[(self.lose[1][i], 1)], (x + s * 8 + 40, y + s * i))
+        for i in range(len(self.lose[-1])):
+            sc.blit(FIG_IM_ST[(self.lose[-1][i], -1)], (x + s * 8 + 50 + fig_sz, y + s * i))
+
 
 
 class UiGame:
     def __init__(self, sc, fn, x, y, s):
         self.bord = UiBord()
-        self.dr_inf = (sc, fn, x, y, s)
+        self.b_inf = x, y, s
+        self.dr_inf = sc, fn, *self.b_inf
 
         self.color = 1
 
+        self.select_pos = None
+
+    def draw(self):
+        if self.select_pos is not None:
+            pos = self.select_pos[0] * self.b_inf[2] + self.b_inf[0], \
+                  self.select_pos[1] * self.b_inf[2] + self.b_inf[1]
+            s = pg.Surface((self.b_inf[2], self.b_inf[2]))
+            s.set_alpha(150)
+            s.fill(yellow)
+            sc.blit(s, pos)
+
+            for i in self.bord.grid[self.select_pos[1]][self.select_pos[0]].go_pos(self.bord):
+                pos = i[1] * self.b_inf[2] + self.b_inf[0], \
+                      i[0] * self.b_inf[2] + self.b_inf[1]
+                s = pg.Surface((self.b_inf[2], self.b_inf[2]))
+                s.set_alpha(150)
+                s.fill(lime)
+                sc.blit(s, pos)
+
     def do_game(self):
         self.bord.draw(*self.dr_inf, self.color)
+        self.draw()
+        if self.bord.is_win(-self.color):
+            return True
+        return False
+
+    def click(self, x, y):
+        xx = (x - self.b_inf[0]) // self.b_inf[2]
+        yy = (y - self.b_inf[1]) // self.b_inf[2]
+        if 0 <= xx < 8 and 0 <= yy < 8:
+            f = self.bord.grid[yy][xx]
+            if f.go_pos(self.bord) and f.color == self.color:
+                self.select_pos = xx, yy
+            elif self.select_pos is not None and (yy, xx) in \
+                    self.bord.grid[self.select_pos[1]][self.select_pos[0]].go_pos(self.bord):
+                self.bord.step(self.color, *self.select_pos[::-1], yy, xx)
+                self.select_pos = None
+                self.color *= -1
+            else:
+                self.select_pos = None
+        else:
+            self.select_pos = None
 
 
 def load_image(name, colorkey=None):  # загружает картинки
@@ -55,23 +106,22 @@ def load_image(name, colorkey=None):  # загружает картинки
 pg.init()
 sc = pg.display.set_mode(size)
 
+pg.display.set_caption('Chess')
 
 FIG_IM_ST = {
-    (Pawn, 1):    pg.transform.scale(load_image('f6.png'), (fig_sz, fig_sz)),
-    (Pawn, -1):   pg.transform.scale(load_image('f12.png'), (fig_sz, fig_sz)),
-    (Rook, 1):    pg.transform.scale(load_image('f5.png'), (fig_sz, fig_sz)),
-    (Rook, -1):   pg.transform.scale(load_image('f11.png'), (fig_sz, fig_sz)),
-    (Knight, 1):  pg.transform.scale(load_image('f4.png'), (fig_sz, fig_sz)),
+    (Pawn, 1): pg.transform.scale(load_image('f6.png'), (fig_sz, fig_sz)),
+    (Pawn, -1): pg.transform.scale(load_image('f12.png'), (fig_sz, fig_sz)),
+    (Rook, 1): pg.transform.scale(load_image('f5.png'), (fig_sz, fig_sz)),
+    (Rook, -1): pg.transform.scale(load_image('f11.png'), (fig_sz, fig_sz)),
+    (Knight, 1): pg.transform.scale(load_image('f4.png'), (fig_sz, fig_sz)),
     (Knight, -1): pg.transform.scale(load_image('f10.png'), (fig_sz, fig_sz)),
-    (Bishop, 1):  pg.transform.scale(load_image('f3.png'), (fig_sz, fig_sz)),
+    (Bishop, 1): pg.transform.scale(load_image('f3.png'), (fig_sz, fig_sz)),
     (Bishop, -1): pg.transform.scale(load_image('f9.png'), (fig_sz, fig_sz)),
-    (King, 1):    pg.transform.scale(load_image('f2.png'), (fig_sz, fig_sz)),
-    (King, -1):   pg.transform.scale(load_image('f8.png'), (fig_sz, fig_sz)),
-    (Queen, 1):   pg.transform.scale(load_image('f1.png'), (fig_sz, fig_sz)),
-    (Queen, -1):  pg.transform.scale(load_image('f7.png'), (fig_sz, fig_sz)),
-    (EmptyF, 0):  pg.transform.scale(load_image('empty.png'), (fig_sz, fig_sz))
+    (King, 1): pg.transform.scale(load_image('f2.png'), (fig_sz, fig_sz)),
+    (King, -1): pg.transform.scale(load_image('f8.png'), (fig_sz, fig_sz)),
+    (Queen, 1): pg.transform.scale(load_image('f1.png'), (fig_sz, fig_sz)),
+    (Queen, -1): pg.transform.scale(load_image('f7.png'), (fig_sz, fig_sz))
 }
-
 
 clock = pg.time.Clock()
 
@@ -84,10 +134,13 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit(0)
+        elif event.type == pg.MOUSEBUTTONDOWN:
+            game.click(*event.pos)
 
-    sc.blit(font.render(str(round(clock.get_fps())), False, red), (width - 50, 30))
-    game.do_game()
+    # sc.blit(font.render(str(round(clock.get_fps())), False, red), (width - 50, 30))
+
+    if game.do_game():
+        game = UiGame(sc, font, *bord_pos, fig_sz)
 
     pg.display.flip()
     clock.tick(FPS)
-
--- ui_settings.py
+++ ui_settings.py
@@ -1,4 +1,4 @@
-size = width, height = 960, 500
+size = width, height = 900, 500
 FPS = 60
 
 fig_sz = 50
@@ -8,3 +8,6 @@ red = (255, 0, 0)
 white = (255, 255, 255)
 gray = (200, 200, 200)
 dk_gray = (128, 128, 128)
+black = (0, 0, 0)
+lime = (100, 255, 20)
+yellow = (200, 200, 20)
