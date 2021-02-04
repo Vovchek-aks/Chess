b81a4aacb6d7adc39bfa7e6bdb5d34646a6e13bf
Vovchek <Vlad.aks03@mail.ru>
2021-02-04 15:43:33 +0500

    добавил слой

--- neuron_net.py
+++ neuron_net.py
@@ -5,5 +5,22 @@ class Neuron:
 
     def get_result(self, inputs):
         out = self.func(sum(inputs))
-        return [out * i for i in self.weights]
+        return (out * i for i in self.weights)
+
+
+class NeuronLayer:
+    def __init__(self, neurons):
+        self.neurons = neurons
+
+    def get_result(self, inputs):
+        inp = [0 for _ in range(len(self.neurons))]
+        for i in inputs:
+            for j in range(len(self.neurons)):
+                inp[j] += i[j]
+
+        return (self.neurons[i].get_result(inp[i]) for i in range(len(self.neurons)))
+
+
+class NeuronNet:
+    pass
 
